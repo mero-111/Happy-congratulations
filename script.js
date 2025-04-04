907d60a94b75a2702ddcacfbd1684c5afee31f73
mero-111 <amrmit12@gmail.com>
2025-04-04 08:57:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function generateCard() {
+    let happy = document.getElementById("Input").value;
+    let name = document.getElementById("nameInput").value;
+    let message = document.getElementById("messageInput").value;
+
+    if (name.trim() === "" || message.trim() === "" || happy.trim() === "") {
+        alert("الرجاء إدخال الاسم والرسالة والمناسبة!");
+        return;
+    }
+
+
+    document.getElementById("cardName").innerText = name;
+    document.getElementById("cardMessage").innerText = message;
+    document.getElementById("title").innerText = happy;
+    document.getElementById("card").classList.remove("hidden");
+    document.getElementById("BTNdownload").classList.remove("hidden");
+}
+
+function downloadCard() {
+    let card = document.getElementById("card");
+
+    html2canvas(card).then(canvas => {
+        let link = document.createElement("a");
+        link.href = canvas.toDataURL("image/png");
+        link.download = "تهنئة عيد الفطر.png";
+        link.click();
+    });
+}
+
+function changeCardColor() {
+    let color = document.getElementById("colorPicker");
+    color.style.backgroundColor = color.value
+    document.getElementById("card").style.backgroundColor = color.value;
+}
+
+function changeFontColorA() {
+    let color = document.getElementById("colorPickerA");
+    color.style.backgroundColor = color.value
+    document.getElementById("title").style.color = color.value;
+    document.getElementById("cardName").style.color = color.value;
+    document.getElementById("cardMessage").style.color = color.value;
+}
